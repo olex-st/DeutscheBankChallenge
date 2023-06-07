2f7f3223671c184fb9b8e8e7b098163bd999e4f8
shoora <keep.calm.b.sharp@gmail.com>
2023-06-07 21:39:21 +0200

    di Module updated

--- app/src/main/java/com/example/deutschebankchallenge/di/UseCaseModule.kt
+++ app/src/main/java/com/example/deutschebankchallenge/di/UseCaseModule.kt
@@ -1,11 +1,16 @@
 package com.example.deutschebankchallenge.di
 
-import com.example.domain.FetchPostsUseCase
-import com.example.domain.GetCommentsUseCase
-import com.example.domain.GetPostsFlowUseCase
-import com.example.domain.LoginUseCase
-import com.example.domain.ToggleFavoriteUseCase
 import com.example.domain.repository.PostsBaseRepository
+import com.example.domain.usecase.FetchPostsUseCase
+import com.example.domain.usecase.GetCommentsUseCase
+import com.example.domain.usecase.GetPostsFlowUseCase
+import com.example.domain.usecase.LoginUseCase
+import com.example.domain.usecase.ToggleFavoriteUseCase
+import com.example.domain.usecase.base.BaseFetchPostsUseCase
+import com.example.domain.usecase.base.BaseGetCommentsUseCase
+import com.example.domain.usecase.base.BaseGetPostsFlowUseCase
+import com.example.domain.usecase.base.BaseLoginUseCase
+import com.example.domain.usecase.base.BaseToggleFavoriteUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,27 +21,27 @@ import dagger.hilt.components.SingletonComponent
 object UseCaseModule {
 
     @Provides
-    fun provideLoginUseCase(postsRepository: PostsBaseRepository): LoginUseCase {
+    fun provideLoginUseCase(postsRepository: PostsBaseRepository): BaseLoginUseCase {
         return LoginUseCase(postsRepository)
     }
 
     @Provides
-    fun provideGetPostsFlowUserCase(postsRepository: PostsBaseRepository): GetPostsFlowUseCase {
+    fun provideGetPostsFlowUserCase(postsRepository: PostsBaseRepository): BaseGetPostsFlowUseCase {
         return GetPostsFlowUseCase(postsRepository)
     }
 
     @Provides
-    fun provideGetCommentsUseCase(postsRepository: PostsBaseRepository): GetCommentsUseCase {
+    fun provideGetCommentsUseCase(postsRepository: PostsBaseRepository): BaseGetCommentsUseCase {
         return GetCommentsUseCase(postsRepository)
     }
 
     @Provides
-    fun provideFetchPostsUseCase(postsRepository: PostsBaseRepository): FetchPostsUseCase {
+    fun provideFetchPostsUseCase(postsRepository: PostsBaseRepository): BaseFetchPostsUseCase {
         return FetchPostsUseCase(postsRepository)
     }
 
     @Provides
-    fun provideToggleFavoriteUseCase(postsRepository: PostsBaseRepository): ToggleFavoriteUseCase {
+    fun provideToggleFavoriteUseCase(postsRepository: PostsBaseRepository): BaseToggleFavoriteUseCase {
         return ToggleFavoriteUseCase(postsRepository)
     }
 }
